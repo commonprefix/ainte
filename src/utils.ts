5d00174e5b9104388705d915fa091149c4d8778c
Dimitris Lamprinos <pkakelas@gmail.com>
2024-09-26 17:54:28 +0300

    Remove needless bash beautifier

--- src/utils.ts
+++ src/utils.ts
@@ -14,23 +14,3 @@ export function getEnv(key: string, _default?: string) {
 export function getPrompt(): string {
   return fs.readFileSync(PROMPT_FILE, "utf8");
 }
-
-// beautify bash command
-export async function logCommand(command: string) {
-  const beautifulScript = command
-    .replace(/(curl)/g, chalk.cyan("$1")) // Highlighting 'curl' command
-    .replace(/(-[sxX])/g, chalk.blue("$1")) // Highlighting options like -s, -X
-    .replace(/(https?:\/\/[^\s]+)/g, chalk.magenta("$1")) // Highlighting URLs
-    .replace(/(-H 'Content-Type: application\/json')/g, chalk.yellow("$1")) // Highlighting headers
-    .replace(/(\{.*?\})/g, chalk.green("$1")) // Highlighting JSON data
-    .replace(/(\| jq '.*?')/g, chalk.red("$1")) // Highlighting jq command
-    .replace(/(\bfor\b|\bdo\b|\bdone\b)/g, chalk.blue("$1")) // Highlighting loop keywords
-    .replace(/;/g, ";\n") // Insert newlines after semicolons
-    .replace(/\b(do|then)\b/g, "$1\n  ") // Indent after 'do' or 'then'
-    .replace(/\bdone\b/g, "\n$&\n") // Newline before and after 'done'
-    .replace(/\n\s*(\b[a-zA-Z_]+\b)/g, "\n  $1"); // Indent other commands inside loops
-
-  // Log the beautifully formatted Bash script
-  console.log(beautifulScript);
-  console.log("\n");
-}
