714542f20eeaa169badd0dc601e6e0a0b7c3b54f
Dimitris Lamprinos <pkakelas@gmail.com>
2024-09-27 15:31:38 +0300

    Add runShellScript fn

---
+++ src/bash.ts
@@ -0,0 +1,26 @@
+import { spawn } from "child_process";
+import type { CommandResult } from "./types";
+
+export function runShellScript(command: string): Promise<CommandResult> {
+    return new Promise((resolve, reject) => {
+        const childProcess = spawn("/bin/sh", ["-c", command]);
+        let stdout = "";
+        let stderr = "";
+
+        childProcess.stdout.on("data", (data: Buffer) => {
+            stdout += data.toString();
+        });
+
+        childProcess.stderr.on("data", (data: Buffer) => {
+            stderr += data.toString();
+        });
+
+        childProcess.on("close", (code: number) => {
+            resolve({ command, output: stdout, error: stderr });
+        });
+
+        childProcess.on("error", (error: Error) => {
+            reject({ command, output: "", error: error.message });
+        });
+    });
+}
