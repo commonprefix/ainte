a5414275e910dd7fdc9bbe4aa24d3bb05abd75e3
Dimitris Lamprinos <pkakelas@gmail.com>
2024-09-28 17:45:25 +0300

    Use null instead of undefined in assistantId getEnv

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { getEnv } from "./utils";
 import { Cli } from "./cli";
 
 const OPENAI_API_KEY = getEnv("OPENAI_API_KEY");
-const OPENAI_ASSISTANT_ID = getEnv("OPENAI_ASSISTANT_ID", undefined);
+const OPENAI_ASSISTANT_ID = getEnv("OPENAI_ASSISTANT_ID", null);
 
 async function main() {
     const assistant = new GPTAssistant(OPENAI_API_KEY);
